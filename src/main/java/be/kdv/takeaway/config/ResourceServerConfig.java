63dfd1363acbd891e727d75b65657ea83a3361e3
Katya de Vries <Jekaterina.DeVries@iadvisegroep.eu>
2018-11-19 11:18:24 +0100

    add ResourceServerConfig

---
+++ src/main/java/be/kdv/takeaway/config/ResourceServerConfig.java
@@ -0,0 +1,76 @@
+package be.kdv.takeaway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    // @formatter:off
+    private static final String[] NON_AUTHORIZED_PATHS = {
+            "/login",
+            "/favicon.ico",
+            // Swagger if implemented
+            "/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/resales/**"
+    };
+    // @formatter:on
+
+    @Value("${spring.data.rest.basePath}")
+    private String resourcePath;
+
+    @Value("${token.signKey}")
+    private String tokenKey;
+
+    @Value("${}resource.authorities")
+    private String[] resourceAuthorities;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .anonymous().disable()
+            .authorizeRequests()
+            .antMatchers(NON_AUTHORIZED_PATHS).permitAll()
+            .antMatchers(HttpMethod.OPTIONS).permitAll()
+            .antMatchers("/**").authenticated()
+            .antMatchers("/" + resourcePath + "/**").hasAnyAuthority(resourceAuthorities)
+            .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenServices(tokenServices());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(tokenKey);
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+}
