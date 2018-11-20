20b68d122a06dfdb133f8525340b92e4f39ad615
Katya de Vries <Jekaterina.DeVries@iadvisegroep.eu>
2018-11-20 13:45:19 +0100

    add ApiError, CusntomRestExceptionHandler in controller

--- src/main/java/be/kdv/takeaway/controller/error_handling/CustomRestExceptionHandler.java
+++ src/main/java/be/kdv/takeaway/controller/error_handling/CustomRestExceptionHandler.java
@@ -4,11 +4,16 @@ import be.kdv.takeaway.exception.BadRequestException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -16,12 +21,27 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
     // general error handler
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<Object> handleBadRequests(BadRequestException ex){
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), "Error occured");
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), "Error occured");
         return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
-        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException ex,
+            final HttpHeaders headers,
+            final HttpStatus status,
+            final WebRequest request
+    ) {
+        List<String> errors = new ArrayList<String>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+
+        return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
     }
 }
