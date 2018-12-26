a06f3486b4425fe36b82a2d189a94c83783b36d6
Bjorn Ongenae <bjornongenae@gmail.com>
2018-12-26 09:49:47 +0100

    coaching TODOs for the Bootstrap class

--- src/main/java/be/kdv/takeaway/bootstrap/Bootstrap.java
+++ src/main/java/be/kdv/takeaway/bootstrap/Bootstrap.java
@@ -1,10 +1,6 @@
 package be.kdv.takeaway.bootstrap;
 
-import be.kdv.takeaway.model.Allergy;
-import be.kdv.takeaway.model.Meal;
-import be.kdv.takeaway.model.Order;
-import be.kdv.takeaway.model.Status;
-import be.kdv.takeaway.model.User;
+import be.kdv.takeaway.model.*;
 import be.kdv.takeaway.repository.MealRepository;
 import be.kdv.takeaway.repository.OrderRepository;
 import be.kdv.takeaway.repository.UserRepository;
@@ -21,8 +17,10 @@ import java.util.Arrays;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
+    // TODO rename this class. Make sure the name tells other developers what it stands for.
 
     private final Logger LOGGER = LoggerFactory.getLogger(Bootstrap.class);
+    // TODO make LOGGER static. Standard Java naming convention is not followed here
 
     private final MealRepository mealRepository;
     private final OrderRepository orderRepository;
@@ -43,86 +41,92 @@ public class Bootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        // TODO remove the Exception that is never thrown
 
         if ((mealRepository.findAll().isEmpty() || orderRepository.findAll().isEmpty())) {
+        // TODO remove obsolete brackets
 
             //clear old broken data
             mongoTemplate.getCollectionNames().forEach(mongoTemplate::dropCollection);
             LOGGER.info("Cleared old data");
 
-                    //creating dummy meals
-                    Meal meal1 = Meal.builder()
-                            .menuNumber(1)
-                            .name("basic")
-                            .description("Basic set")
-                            .allergies(new ArrayList<Allergy>(Arrays.asList(Allergy.NUTS, Allergy.LACTOSE)))
-                            .build();
+            //creating dummy meals
+            Meal meal1 = Meal.builder()
+                             .menuNumber(1)
+                             .name("basic")
+                             .description("Basic set")
+                             .allergies(new ArrayList<Allergy>(Arrays.asList(Allergy.NUTS, Allergy.LACTOSE)))
+                             .build();
+            // TODO remove type argument
 
-                    Meal meal2 = Meal.builder()
-                            .menuNumber(2)
-                            .name("medium")
-                            .description("Peking duck")
-                            .allergies(new ArrayList<Allergy>(Arrays.asList(Allergy.GLUTEN, Allergy.NUTS)))
-                            .build();
+            Meal meal2 = Meal.builder()
+                             .menuNumber(2)
+                             .name("medium")
+                             .description("Peking duck")
+                             .allergies(new ArrayList<Allergy>(Arrays.asList(Allergy.GLUTEN, Allergy.NUTS)))
+                             .build();
+            // TODO remove type argument
 
-                    Meal meal3 = Meal.builder()
-                            .menuNumber(2)
-                            .name("Lux")
-                            .description("Chef's speciality")
-                            .allergies(new ArrayList<Allergy>(Arrays.asList(Allergy.GLUTEN, Allergy.SHELLFISH)))
-                            .build();
+            Meal meal3 = Meal.builder()
+                             .menuNumber(2)
+                             .name("Lux")
+                             .description("Chef's speciality")
+                             .allergies(new ArrayList<Allergy>(Arrays.asList(Allergy.GLUTEN, Allergy.SHELLFISH)))
+                             .build();
+            // TODO remove type argument
 
-                    mealRepository.save(meal1);
-                    mealRepository.save(meal2);
-                    mealRepository.save(meal3);
-                    LOGGER.info("3 meals created");
+            mealRepository.save(meal1);
+            mealRepository.save(meal2);
+            mealRepository.save(meal3);
+            LOGGER.info("3 meals created");
 
-                    //creating dummy orders
-                    Order order1 = Order.builder()
+            // TODO Use the IntelliJ warnings
+            //creating dummy orders
+            Order order1 = Order.builder()
                                 .customerName("Peter Pan")
                                 .meals(new ArrayList<Meal>(Arrays.asList(meal1, meal2)))
                                 .status(Status.REQUESTED)
                                 .createdAt(Instant.now())
                                 .readyAt(Instant.now()
-                                        .plus(30, ChronoUnit.MINUTES))
+                                                .plus(30, ChronoUnit.MINUTES))
                                 .build();
-                    Order order2 = Order.builder()
+            Order order2 = Order.builder()
                                 .customerName("Caption Hook")
                                 .meals(new ArrayList<Meal>(Arrays.asList(meal2, meal3)))
                                 .status(Status.PREPARING)
                                 .createdAt(Instant.now())
                                 .readyAt(Instant.now()
-                                        .plus(30, ChronoUnit.MINUTES))
+                                                .plus(30, ChronoUnit.MINUTES))
                                 .build();
-                    Order order3 = Order.builder()
+            Order order3 = Order.builder()
                                 .customerName("Wendy")
                                 .meals(new ArrayList<Meal>(Arrays.asList(meal3)))
                                 .status(Status.REQUESTED)
                                 .createdAt(Instant.now())
                                 .readyAt(Instant.now()
-                                        .plus(30, ChronoUnit.MINUTES))
+                                                .plus(30, ChronoUnit.MINUTES))
                                 .build();
 
-                        orderRepository.save(order1);
-                        orderRepository.save(order2);
-                        orderRepository.save(order3);
-                        LOGGER.info("3 orders created");
+            orderRepository.save(order1);
+            orderRepository.save(order2);
+            orderRepository.save(order3);
+            LOGGER.info("3 orders created");
         }
 
-        if(userRepository.findAll().isEmpty()){
+        if (userRepository.findAll().isEmpty()) {
 
             //create users
             User user1 = User.builder()
-                    .username("Hu Awai")
-                    .password("$2a$10$jXpENQlTEQ5ecn90ugTyxOAotZOox7o3Kyign2NT//RZZCokPN5h2")
-                    .email("info@takeaway.be")
-                    .enabled(true)
-                    .build();
+                             .username("Hu Awai")
+                             .password("$2a$10$jXpENQlTEQ5ecn90ugTyxOAotZOox7o3Kyign2NT//RZZCokPN5h2")
+                             .email("info@takeaway.be")
+                             .enabled(true)
+                             .build();
             User user2 = User.builder()
-                    .username("So Ni")
-                    .password("$2a$10$jXpENQlTEQ5ecn90ugTyxOAotZOox7o3Kyign2NT//RZZCokPN5h2")
-                    .enabled(true)
-                    .build();
+                             .username("So Ni")
+                             .password("$2a$10$jXpENQlTEQ5ecn90ugTyxOAotZOox7o3Kyign2NT//RZZCokPN5h2")
+                             .enabled(true)
+                             .build();
 
             userRepository.save(user1);
             userRepository.save(user2);
